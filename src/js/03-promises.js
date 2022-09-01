f4685972f9b55c599d9904ece9f9854fcb48c57e
VasylMolnar <vasylmolnar281@gmail.com>
2022-09-01 11:14:40 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,13 @@
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  console.log(event);
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
