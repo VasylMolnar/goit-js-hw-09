c863be1afddb259693f8ecad1888ea7567cec0f4
VasylMolnar <108887823+VasylMolnar@users.noreply.github.com>
2022-09-01 09:56:33 +0000

    Deploying to gh-pages from @ VasylMolnar/goit-js-hw-09@8b8f34bd7856ed3c1cea262a89e198d33a20f4eb 🚀

--- 03-promises.83e5611a.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector(".form")}).form.addEventListener("submit",(e=>{e.preventDefault(),console.log(e)}));
-//# sourceMappingURL=03-promises.83e5611a.js.map
--- 03-promises.baec9d20.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(e=>{e.preventDefault(),console.log(e)}));
-//# sourceMappingURL=03-promises.baec9d20.js.map
