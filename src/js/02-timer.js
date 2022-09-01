e7d56ce00b1ef5b4fe9a938702cf5fab49ac0391
VasylMolnar <vasylmolnar281@gmail.com>
2022-09-01 08:48:02 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,14 @@
 import flatpickr from 'flatpickr';
 import Notiflix from 'notiflix';
+import 'flatpickr/dist/flatpickr.min.css';
+import 'flatpickr/dist/themes/dark.css';
+let selectedTime = null;
+
+const refs = {
+  inputDate: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
