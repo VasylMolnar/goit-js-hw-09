8b8f34bd7856ed3c1cea262a89e198d33a20f4eb
VasylMolnar <vasylmolnar281@gmail.com>
2022-09-01 12:56:01 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const refs = {
   form: document.querySelector('.form'),
 };
 
 refs.form.addEventListener('submit', event => {
   event.preventDefault();
+  let delay = Number(event.currentTarget.delay.value);
+  const step = Number(event.currentTarget.step.value);
+  const amount = Number(event.currentTarget.amount.value);
+  //console.log(event.currentTarget.elements);
 
-  console.log(event);
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, delay)
+      .then(value => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+            useIcon: false,
+          });
+        }, delay);
+      })
+      .catch(value => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+            useIcon: false,
+          });
+        }, delay);
+      });
+    delay += step;
+  }
 });
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const objectPromise = { position, delay };
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(objectPromise);
+    }
+    reject(objectPromise);
+  });
 }
